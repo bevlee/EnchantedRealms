86992dea9334808268982bb576e483942b39c5a1
bev <bevan.lee@live.com.au>
2024-04-20 23:32:24 +1000

    filled out draft representation for a card

---
+++ CardBase.gd
@@ -0,0 +1,69 @@
+extends MarginContainer
+
+@onready var cardDatabase = preload("res://CardsDatabase.gd")
+
+var cardName = "Knight"
+@onready var cardInfo = cardDatabase.DATA[cardName]
+@onready var cardImg = str("res://Assets/Cards/Units/", cardName, ".png")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var cardSize = size
+	var level = 10
+	print(cardImg)
+	$Border.scale *= cardSize / $Border.texture.get_size()
+	$Card.texture = load(cardImg)
+	$Card.scale *= cardSize/$Card.texture.get_size()
+	
+	$CardBars/TopBar/MidSection/Label.text = cardName
+	
+	
+	#load in the type icon
+	var cardType = cardInfo[0]
+	var cardTypeIcon = str("res://Assets/Cards/Type/", cardType, ".png")
+	print(cardTypeIcon)
+	$CardBars/TopBar/TypeIcon/TypeIconSprite.texture = load(cardTypeIcon)
+	$CardBars/TopBar/TypeIcon/TypeIconSprite.scale *= $CardBars/TopBar/TypeIcon.custom_minimum_size / $CardBars/TopBar/TypeIcon/TypeIconSprite.texture.get_size()
+	
+	
+	# Load in the stars 
+	var stars = cardInfo[1]
+	for n in range(stars):
+		var starImg = str("res://Assets/Cards/Misc/star.png")
+		var starSprite = Sprite2D.new()
+		starSprite.texture = load(starImg)
+		starSprite.position = $CardBars/TopBar/MidSection/MarginContainer/Star.get_position()
+		starSprite.position.y += 10
+		starSprite.position.x += 15*n + 5
+		starSprite.scale *= Vector2(15, 15) / starSprite.texture.get_size()
+		$CardBars/TopBar/MidSection/MarginContainer/Star.add_child(starSprite)
+		
+	
+	# Set Skills
+	$CardBars/Skill1.text = cardInfo[4] + "    "
+	$CardBars/Skill2.text = cardInfo[5] + "    "
+	$CardBars/Skill3.text = cardInfo[6] + "    "
+	
+	
+	# Set Cost
+	var cost = cardInfo[2]
+	$CardBars/TopBar/MarginContainer/Cost.text = str(cost)
+	
+	# Set Level
+	$CardBars/LvHPRow/Level/LevelLabel.text = str(level)
+	
+	# Set ATK
+	var atk = cardInfo[8] + cardInfo[11]*level
+	$CardBars/ATKWeightRow/ATK/ATKLabel.text = "          " + str(atk)
+	
+	# Set Wait
+	var wait = cardInfo[3]
+	$CardBars/ATKWeightRow/WaitTime/WaitLabel.text = "  " + str(wait)
+	
+	# Set hp
+	var hp = cardInfo[9] + cardInfo[12]*level
+	$CardBars/LvHPRow/HP/HPLabel.text = "     " + str(hp)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ CardsDatabase.gd
@@ -0,0 +1,16 @@
+
+
+enum {Footman, Archer}
+
+# Type, Stars, Cost, Wait, Skill1, Skill2, Skill3, Skill4, baseATK, baseHP, maxLevel, ATKGrowth, HPGrowth
+const DATA = {
+	"Footman" : 
+		["Earth", 1, 4, 2, 'Earth Shatter 4', 'Defender 3', 'Berserk' ,'Skill 4', 70, 350, 10, 15, 25 ],
+	"Archer" : 
+		["Air", 1, 6, 0, 'Snipe 2', 'Tactical Retreat', 'Volley' ,'Skill 4', 120, 280, 10, 25, 10 ],
+	"Guardian" : 
+		["Earth", 2, 4, 0, 'Snipe 2', 'Tactical Retreat', 'Volley' ,'Skill 4', 100, 450, 10, 20, 25 ],
+	"Knight" : 
+		["Earth", 5, 4, 0, 'Snipe 2', 'Tactical Retreat', 'Volley' ,'Skill 4', 220, 480, 10, 30, 25 ],
+		
+}
