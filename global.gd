b7bb87776959ebb66c039cc3633b4e4b5a741eee
bev <bevan.lee@live.com.au>
2024-12-02 23:31:46 +1100

    add player 2

--- global.gd
+++ global.gd
@@ -6,5 +6,11 @@ var playerStateMachine = {
 	"deck" = ["Footman", "Footman", "Archer", "Archer"],
 	"cardList" = [],
 }
+
+var playerStateMachine2 = {
+	"deck" = ["Footman", "Footman", "Archer", "Archer"],
+	"cardList" = [],
+}
 func _ready():
 	Global.playerStateMachine = self.playerStateMachine
+	Global.playerStateMachine2 = self.playerStateMachine2
