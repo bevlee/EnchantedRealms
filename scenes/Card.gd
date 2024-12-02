1a425fdb88b1c41c0107bb5436e330b20649f909
bev <bevan.lee@live.com.au>
2024-12-02 23:32:03 +1100

    make new parent scene to cover all card view types

---
+++ scenes/Card.gd
@@ -0,0 +1,228 @@
+extends Node2D
+
+class_name Card
+
+signal dead
+
+@onready var cardDatabase = load("res://CardsDatabase.gd")
+@onready var skillDatabase = preload("res://SkillsDatabase.gd")
+
+var skillsDatabase = preload("res://SkillsDatabase.gd")
+var passive_skills = skillsDatabase.PASSIVE_SKILLS
+
+# either detail, combat or hand 
+var view : String = "detail" :
+	get:
+		return view
+	set(val):
+		match val:
+			"detail":
+				print("detail view")
+				$DetailView.visible = true
+				$CombatView.visible = false
+				$CompactView.visible = false
+				base_scene = $DetailView
+				
+			"combat":
+				print("combat view")
+				$DetailView.visible = false
+				$CombatView.visible = true
+				$CompactView.visible = false
+				base_scene = $CombatView
+			"compact": # hand + graveyard
+				print("compact view")
+				$DetailView.visible = false
+				$CombatView.visible = false
+				$CompactView.visible = true
+				base_scene = $CompactView
+		view = val
+		
+
+var base_scene: Node2D
+var default_cardName = "Footman"
+var cardName
+var level = 1
+var wait_timer : int :
+	get: 
+		return wait_timer
+	set(val):
+		wait_timer = max(0, val)
+		if view == "compact":
+			base_scene.get_node("CardWait/WaitLabel").text = str(wait_timer)
+		if wait_timer == 0:
+			var playableBorder = str("res://Assets/Cards/Borders/square_border_playable.png")
+			base_scene.get_node("Sprites/Border").texture = load(playableBorder)
+			
+		
+var card_atk :
+	set(val):
+		card_atk = val
+		if view != "compact":
+			base_scene.get_node("Attributes/Attack/AttackLabel").text = "ATK: " + str( val)
+var default_hp
+var card_hp: 
+	get: 
+		return card_hp
+	set(val):
+		var new_hp: int = max(0, val)
+		card_hp = new_hp
+		if view != "compact":
+			var hp_label = base_scene.get_node("Attributes/HP/HPLabel")
+			hp_label.text = str(new_hp)
+			# set black for default
+			if new_hp == default_hp:
+				hp_label.add_theme_color_override("font_color", Color(0,0,0,1)) 
+				
+			# set red if lower than default
+			elif new_hp < default_hp:
+				hp_label.add_theme_color_override("font_color", Color(1,0,0,1))
+			# set green if higher than default
+			elif new_hp > default_hp:
+				hp_label.add_theme_color_override("font_color", Color(0,1,0,1))
+			if new_hp == 0:
+				dead.emit()
+		
+		
+		
+var skill1
+var skill2
+var skill3
+# buffs and debuffs
+var applied_effects: Dictionary
+const CARD_SIZES = {
+	"combat": Vector2(172,220),
+	"detail": Vector2(240,320),
+	"compact": Vector2(100,100),
+	"sprite": Vector2(1000,1500),
+	"border": Vector2(1150,1660)
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if cardName == null:
+		cardName = default_cardName
+	load_card(cardName, level, view)
+	base_scene.get_node("Sprites/Card").scale = CARD_SIZES["sprite"] / base_scene.get_node("Sprites/Card").texture.get_size()
+	base_scene.get_node("Sprites/Border").scale = CARD_SIZES["border"] / base_scene.get_node("Sprites/Border").texture.get_size() 
+	var scaled_base_size = CARD_SIZES["border"]
+	var size = scaled_base_size
+	print(size)
+	base_scene.scale = CARD_SIZES[view] / CARD_SIZES["border"]
+	base_scene.position = CARD_SIZES[view] / 2
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func take_action(action: String) -> void:
+	print("starting action for card" + cardName)
+	set_active()
+	$Effects/ActionText.text = action
+	var tween = create_tween()
+	$Effects/ActionText.position=Vector2(0,225)
+	$Effects/ActionText.visible=true
+	$Effects/ActionText.modulate=Color(1,1,1,1)
+	tween.tween_property($Effects/ActionText, "position", Vector2(0,-100), 1.0)
+	tween.tween_property($Effects/ActionText, "modulate", Color(1,1,1,0), 0.5)
+	await tween.finished
+	$Effects/ActionText.visible=false
+	set_inactive()
+	print("finishing action for card" + cardName)
+	
+func set_active():
+	var a = base_scene.get_node("Sprites/Border").material
+	base_scene.get_node("Sprites/Border").material.set_shader_parameter("outline_color", Color(255,0,0,1)) 
+	
+func set_inactive():
+
+	base_scene.get_node("Sprites/Border").material.set_shader_parameter("outline_color", Color(0,0,0,1)) 
+
+func load_card(cardName = "Archer", level = 1, view="combat"):
+	var cardImg
+	var cardInfo = cardDatabase.DATA[cardName]
+	skill1 = cardInfo[4]
+	skill2 = cardInfo[5]
+	skill3 = cardInfo[6]
+	
+	
+	## Set Level
+	#base_scene.get_node("Attributes/HP/HPLabel").text = str(level)
+	
+	# Set ATK
+	var atk = cardInfo[8] + cardInfo[11]*level
+	card_atk = atk
+	
+	# Set hp
+	default_hp = cardInfo[9] + cardInfo[12]*level
+	#card_hp = default_hp
+	card_hp = default_hp
+	# Set wait timer
+	wait_timer = cardInfo[3]
+	
+	if view == "compact":
+		cardImg = str("res://Assets/Cards/UnitIcons/", cardName, ".png")
+	else:
+		cardImg = str("res://Assets/Cards/Units/", cardName, ".png")
+		
+		#load in the type icon
+		var cardType = cardInfo[0]
+		var cardTypeIcon = str("res://Assets/Cards/Type/", cardType, ".png")
+		print(cardTypeIcon)
+		var a = base_scene.get_node("Attributes").get_children()
+		print(a)
+		base_scene.get_node("Attributes/Type/TypeIcon").texture = load(cardTypeIcon)
+		
+		base_scene.get_node("Attributes/Attack/AttackLabel").text = "ATK: " + str(atk)
+		base_scene.get_node("Attributes/HP/HPLabel").text = "HP: " + str(default_hp)
+
+	print(cardImg)
+	#$Border.scale *= cardSize / $Border.texture.get_size()
+	base_scene.get_node("Sprites/Card").texture = load(cardImg)
+	#$Card.scale *= cardSize/$Card.texture.get_size()
+	
+	
+
+	
+func get_skills() ->  Array:
+	return [skill1, skill2, skill3]
+
+func modify(source: Object, effect_type: String, hp_change: int = 0, atk_change: int = 0, add_effect: String = "", remove_effect: String = "", effect_duration = 0):
+	var modified_hp_change = hp_change
+	match effect_type:
+		"physical":
+			for skill in get_skills():
+				if skill in passive_skills:
+					match skill: 
+						"Defender":
+							modified_hp_change = modified_hp_change / 2 
+						"Dodge":
+							# 50% chance to dodge it
+							if randf() < 0.5:
+								modified_hp_change = 0
+		"buff", "debuff":
+			if add_effect != "":
+				if add_effect in applied_effects:
+					applied_effects[add_effect].duration += effect_duration
+				else:
+					applied_effects[add_effect] = { 
+						"duration": effect_duration,
+						"hp_change": hp_change,
+						"atk_change": atk_change
+					}
+			elif remove_effect != "":
+				var res = applied_effects.erase(remove_effect)
+				print(remove_effect + " was removed: " + str(res))
+	if modified_hp_change != 0:
+		card_hp += modified_hp_change
+		
+func tick():
+	for key in applied_effects:
+		var duration = applied_effects[key]
+		if duration <= 0:
+			remove_effect(key)
+			
+func remove_effect(key: String):
+	card_atk += applied_effects[key]["atk_change"]
+	card_hp += applied_effects[key]["hp_change"]
+	applied_effects.erase(key)
+	
